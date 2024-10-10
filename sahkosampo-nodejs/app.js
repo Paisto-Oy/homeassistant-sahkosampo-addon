bf4ba96bfdfa1a34a684d5681bbaeb26160c223d
John-Robert <robert.yrjola@gmail.com>
2024-10-10 22:39:04 +0300

    api test

--- sahkosampo-nodejs/app.js
+++ sahkosampo-nodejs/app.js
@@ -1,14 +1,45 @@
+require('dotenv').config();
 // app.js
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9467;
+const SUPERVISOR_TOKEN = process.env.SUPERVISOR_TOKEN;
 
 // Simple route
 app.get('/', (req, res) => {
-    res.send('Hello from Home Assistant Node.js Addon!');
+    sendHaMessage({ "type": "get_states" });
+    axios.get(`${HA_ROOT_URL}/api/states`,
+        {
+            headers: {
+                "Authorization": `Bearer ${SUPERVISOR_TOKEN}`
+            }
+        }
+    ).then(result => {
+        res.json(result.data)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
 });
+
+app.get('/api/ha/states/', (req, res) => {
+    sendHaMessage({ "type": "get_states" });
+    axios.get(`${HA_ROOT_URL}/api/states`,
+        {
+            headers: {
+                "Authorization": `Bearer ${SUPERVISOR_TOKEN}`
+            }
+        }
+    ).then(result => {
+        res.json(result.data)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
 
 // Start server
 app.listen(port, () => {
     console.log(`Addon is running on port ${port}`);
+    console.log(`Supervisor token ${SUPERVISOR_TOKEN}`);
 });
