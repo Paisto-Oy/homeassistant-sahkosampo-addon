52db9c44143264eb2bccf0484919713d3447478c
John-Robert <robert.yrjola@gmail.com>
2024-10-10 12:20:51 +0300

    Changed port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9467;
 
 // Simple route
 app.get('/', (req, res) => {
